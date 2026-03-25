7ae9ea1954b329ee596ca097c81d4a54ae8f2190
ino-cent <paulinnocentojochenemi@gmail.com>
2026-03-25 21:15:53 +0100

    update

--- ElumPro/ElumPro/settings.py
+++ ElumPro/ElumPro/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'DEPLOY_STATIC')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'DEPLOY_STATIC')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 STATICFILES_DIRS = [
--- ElumPro/build.sh
+++ ElumPro/build.sh
@@ -1,19 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
 
-echo "--- BUILD START ---"
+# Install dependencies
+pip install -r requirements.txt
 
-# 1. Install Python dependencies
-# Using -m pip is the safest way on Vercel
-python3.12 -m pip install -r requirements.txt
+# Run migrations (Optional but recommended for Render)
+python manage.py migrate
 
-echo "--- COLLECTING STATIC FILES ---"
-# 2. Collect Static Files (This is what fixes your CSS)
-# We use --noinput so it doesn't wait for you to type 'yes'
-python3.12 manage.py collectstatic --noinput --clear
-
-echo "--- RUNNING MIGRATIONS ---"
-# 3. Database Migrations
-# Note: If this fails, check your DATABASE_URL port (should be 6543)
-python3.12 manage.py migrate --noinput
-
-echo "--- BUILD END ---"
+# Collect static files into the folder Render will serve
+python manage.py collectstatic --noinput --clear
